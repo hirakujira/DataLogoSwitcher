ed30886b6f814c46f75718950307eaea796a2780
Hiraku <dev@hiraku.tw>
2019-09-26 21:38:53 +0800

    Add README.md

--- DataLogoSwitcherSettings/DataLogoSwitcher.h
+++ DataLogoSwitcherSettings/DataLogoSwitcher.h
@@ -6,11 +6,6 @@
 #define UserDefaultsChangedNotification "tw.hiraku.datalogoswitcher"
 #define SettingsPath @"/var/mobile/Library/Preferences/tw.hiraku.datalogoswitcher.plist"
 
-#define kCFCoreFoundationVersionNumber_iOS_5_0 675.00
-#define kCFCoreFoundationVersionNumber_iOS_6_0 793.00
-#define kCFCoreFoundationVersionNumber_iOS_7_0 847.20
-#define VERSION @"0.0.2"
-
 @interface PSSpecifier (DataLogoSwitcher)
 - (void)setIdentifier:(NSString *)identifier;
 @end
